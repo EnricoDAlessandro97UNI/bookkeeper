97be2632efe4e564228f843ebc46155f98b5281c
EnricoDAlessandro97UNI <enrico.dalessandro@alumni.uniroma2.eu>
2023-06-07 12:08:34 +0200

    Incremented jacoco coverage WriteCache put method #2

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTests.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTests.java
@@ -27,6 +27,7 @@ public class WriteCacheTests {
 	private static final long EXISTING_ENTRY_ID = 1;
 	private static final long NON_EXISTING_ENTRY_ID = 0;
 	private static final long MAX_CACHE_SIZE = 4*1024;
+	private static final int MAX_SEGMENT_SIZE = 1024;
 
 	
 	@RunWith(Parameterized.class)
@@ -38,61 +39,75 @@ public class WriteCacheTests {
 		private boolean expected;
 		
 		private WriteCache cache;
+		private boolean existsMaxSegmentSize;
 		
 		//costruttore
-	    public WriteCachePutTest(long ledgerId, long entryId, Integer entrySize, boolean expected) {
-	        configure(ledgerId, entryId, entrySize, expected);
+	    public WriteCachePutTest(long ledgerId, long entryId, Integer entrySize, boolean expected, boolean existsMaxSegmentSize) {
+	        configure(ledgerId, entryId, entrySize, expected, existsMaxSegmentSize);
 	    }
 
-	    public void configure(long ledgerId, long entryId, Integer entrySize, boolean expected) {
+	    public void configure(long ledgerId, long entryId, Integer entrySize, boolean expected, boolean existsMaxSegmentSize) {
 	        this.expected = expected;
 	        this.ledgerId = ledgerId;
 	        this.entryId = entryId;
 	        if(entrySize != null)
 	            this.entry = Unpooled.wrappedBuffer(new byte[entrySize]);
 	        else this.entry = null;
+	        this.existsMaxSegmentSize = existsMaxSegmentSize;
 	    }
 
-		@Parameterized.Parameters
+	    @Parameterized.Parameters
 	    public static Collection<?> getTestParameters() {
 			return Arrays.asList(new Object[][] {
-	    		// LEDGER_ID        ENTRY_ID                ENTRY_SIZE		EXPECTED
+	    		// LEDGER_ID        ENTRY_ID                ENTRY_SIZE		EXPECTED	MAX_SEGMENT_SIZE
 				
 				// configurazione valida
-	        	{  LEDGER_ID,		EXISTING_ENTRY_ID,		1024,			true    }, // ok
+	        	{  LEDGER_ID,		EXISTING_ENTRY_ID,		1024,			true,    	false  }, // ok
 	        	
 	        	// entry > maxCacheSize
-	        	{  LEDGER_ID,		EXISTING_ENTRY_ID,		6*1024,			false   }, 
+	        	{  LEDGER_ID,		EXISTING_ENTRY_ID,		6*1024,			false,   	false  }, 
 	        	
 	        	// entry null
-	        	{  LEDGER_ID,       EXISTING_ENTRY_ID,      null,       	false   }, 
+	        	{  LEDGER_ID,       EXISTING_ENTRY_ID,      null,       	false,   	false  }, 
 	        	
 	        	// entryId non esistente e varianti
-	        	{  LEDGER_ID,       NON_EXISTING_ENTRY_ID,  1024,       	true    }, // ok
-	        	{  LEDGER_ID,       NON_EXISTING_ENTRY_ID,  6*1024,    		false   },
-	        	{  LEDGER_ID,       NON_EXISTING_ENTRY_ID,  null,        	false   },
+	        	{  LEDGER_ID,       NON_EXISTING_ENTRY_ID,      				1024,       	true,    	false  }, // ok
+	        	{  LEDGER_ID,       NON_EXISTING_ENTRY_ID,      				6*1024,    		false,   	false  },	
+	        	{  LEDGER_ID,       NON_EXISTING_ENTRY_ID,  					null,        	false,  	false  },
 	        	
 	        	// entryId negativo e varianti
-	        	{  LEDGER_ID,      -1,						1024,       	true   }, // ok, ma non viene fatto un controllo su entryId > 0 
-	        	{  LEDGER_ID,      -1,						6*1024,    		false   },
-	        	{  LEDGER_ID,      -1,      				null,        	false   },
+	        	{  LEDGER_ID,      -1,						1024,       	true,    	false  }, // ok, ma non viene fatto un controllo su entryId > 0 
+	        	{  LEDGER_ID,      -1,						6*1024,    		false,   	false  },
+	        	{  LEDGER_ID,      -1,      				null,        	false,   	false  },
 	        	
 	        	// ledgerId negativo e varianti
-	        	{ -1,             	EXISTING_ENTRY_ID,      1024,       	false   }, 
-	        	{ -1,             	EXISTING_ENTRY_ID,      6*1024,      	false   },
-	        	{ -1,  				EXISTING_ENTRY_ID,      null,        	false   },
-	        	{ -1,  	   			NON_EXISTING_ENTRY_ID,  1024,       	false   },
-	        	{ -1,  	   			NON_EXISTING_ENTRY_ID,  6*1024,      	false   },
-	        	{ -1,  				NON_EXISTING_ENTRY_ID,  null,        	false   },
-	        	{ -1,  			   -1,               		1024,       	false   },
-	        	{ -1,  			   -1,               		6*1024,      	false   },
-	        	{ -1,  			   -1,               		null,        	false   }
+	        	{ -1,             	EXISTING_ENTRY_ID,      1024,       	false,   	false  },
+	        	{ -1,             	EXISTING_ENTRY_ID,      6*1024,      	false,   	false  },
+	        	{ -1,  				EXISTING_ENTRY_ID,      null,        	false,   	false  },
+	        	
+	        	{ -1,  	   			NON_EXISTING_ENTRY_ID,  1024,       	false,   	false  },
+	        	{ -1,  	   			NON_EXISTING_ENTRY_ID,  6*1024,      	false,   	false  },
+	        	{ -1,  				NON_EXISTING_ENTRY_ID,  null,        	false,   	false  },
+	        	
+	        	{ -1,  			   -1,               		1024,       	false,   	false  },
+	        	{ -1,  			   -1,               		6*1024,      	false,   	false  },
+	        	{ -1,  			   -1,               		null,        	false,   	false  },
+	        	
+	        	// for coverage
+	        	{  1,  			    NON_EXISTING_ENTRY_ID,  2*1024,        	false,   	true   }, // jacoco: maxSegSize - localOffset < size
+	        	{  1,  			    NON_EXISTING_ENTRY_ID,  1024,        	true,   	true   }  // jacoco: maxSegSize - localOffset = size
 	        });
 	    }
 
 		@Before
 		public void setUp() {
-			cache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, MAX_CACHE_SIZE); // 10KB di cache
+			if (existsMaxSegmentSize) {
+				cache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, MAX_CACHE_SIZE, MAX_SEGMENT_SIZE); 
+			} 
+			else {
+				cache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, MAX_CACHE_SIZE);
+			}
+			
 			if (this.ledgerId == LEDGER_ID) {
 				ByteBuf initialEntry = Unpooled.wrappedBuffer("initial-entry".getBytes());
 				initialEntry.writerIndex(initialEntry.capacity());
@@ -110,7 +125,7 @@ public class WriteCacheTests {
 				actual = cache.get(ledgerId, entryId).equals(entry);
 			} catch (NullPointerException | IllegalArgumentException e) {
 				actual = false;
-			} 
+			}
 			Assert.assertEquals(this.expected, actual);
 		}
 		
